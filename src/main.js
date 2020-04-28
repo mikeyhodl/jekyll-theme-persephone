7af73c2393875f197615f3856ea6e39b6d6724d3
张时雨 <zhangshiyu@datatang.com>
2020-04-28 13:28:41 +0800

    1. 添加turbolink; 2. 调整评论框内容文本样式。

--- src/main.js
+++ src/main.js
@@ -1,47 +1,32 @@
-import Slider from './components/Slider.js'
-import Summary from './components/Summary.js'
-import Comment from './components/Comment.js'
-import Archive from './components/Archive.js'
-
 import './css/main.scss'
 
-if ( document.getElementById("bookSummary") ) {
-  new Summary()
-}
+import Slider from './components/Slider'
+import Summary from './components/Summary'
+import Comment from './components/Comment'
+import Archive from './components/Archive'
 
-if ( document.getElementById("sectionContainer") ) {
-  new Slider()
-}
+import initMermaid from './helpers/mermaid'
 
-if ( document.getElementById("postArchive") ) {
-  new Archive()
-}
+import Turbolinks from "turbolinks"
 
-if ( document.getElementById("commentContainer") ) {
-  new Comment()
-}
+Turbolinks.start()
 
-initMermaid();
-
-/**
- * Get all of the mermaid code block and transform them into the format can be parsed by mermaid.
- */
-function initMermaid() {
-  let codes = document.getElementsByClassName("language-mermaid");
-
-  while( codes.length > 0 ) {
-    let code = codes[0];
-    let content = code.innerText;
-
-    let chart = document.createElement("div");
-    chart.className = "mermaid";  
-    chart.innerHTML = content;
-
-    let p = code.parentNode;
-    while( p.tagName != "DIV" ) {
-      p = p.parentNode;
-    }
-
-    p.replaceChild(chart, code.parentNode);
+document.addEventListener("turbolinks:load", function() {
+  if ( document.getElementById("bookSummary") ) {
+    new Summary()
   }
-}
+  
+  if ( document.getElementById("sectionContainer") ) {
+    new Slider()
+  }
+  
+  if ( document.getElementById("postArchive") ) {
+    new Archive()
+  }
+  
+  if ( document.getElementById("commentContainer") ) {
+    new Comment()
+  }
+  
+  initMermaid();
+})
